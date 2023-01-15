7d9c87a9de14d1832b87aebab9bc5a20bf8f5ac1
Shaira Alam <shaira.alam19@gmail.com>
2023-01-15 02:43:21 -0700

    Navigation

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,26 +1,43 @@
+import * as React from 'react';
+import { NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Button, Alert, Platform } from 'react-native';
 
-export default function App() {
-  const handlePress = () => {
-    console.log("Text Pressed")
-  }
-  
+function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Text numberOfLines={1} onPress={handlePress}>Coach Connect</Text> */}
-      <Image style={styles.logo} source={require('./assets/logo.png')}/>
       {/* <StatusBar style="auto" /> */}
+      {/* <Text numberOfLines={1} onPress={handlePress}>Coach Connect</Text> */}
+      <Image style={styles.logo} source={require('./assets/logo_transparent.png')}/>
+      <Button style={styles.sign_up} title="Sign Up" 
+        onPress={() => Alert.alert("Error", "Make sure to sign up", [
+          { text: "Yes", onPress: () => console.log("Yes")},
+          { text: "No", onPress: () => console.log("No")}
+        ])}/>
     </SafeAreaView>
+  );
+}
+
+export default function App() {  
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // {HomeScreen()}
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#702FBF',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? 20 : 0 
   },
   tinyLogo: {
     width: 50,
@@ -30,4 +47,7 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
   },
+  sign_up: {
+    backgroundColor: '#EDB951'
+  }
 });
